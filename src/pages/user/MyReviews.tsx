39f99cf2d64fe654a4711c3ec18d66a268405fb0
Gokul G Bhattathiri <gokulgb2020@gmail.com>
2025-07-06 12:10:24 +0530

    Move MyReviews component to profile directory
    Renamed MyReviews.tsx from components to components/profile for better organization. Updated import path in MyReviewsPage to reflect the new location.

--- src/pages/user/MyReviews.tsx
+++ src/pages/user/MyReviews.tsx
@@ -1,7 +1,7 @@
 
 import Header from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
-import MyReviews from "@/components/MyReviews";
+import MyReviews from "@/components/profile/MyReviews";
 
 const MyReviewsPage = () => {
   return (
